5fb269f03b62c3ad5e8e970cc735d41a149ea71e
GuGuMur <2221533105@qq.com>
2024-10-07 15:27:14 +0800

    chore: clean unused config

--- nonebot_plugin_skland_arksign/did/api.py
+++ nonebot_plugin_skland_arksign/did/api.py
@@ -3,9 +3,8 @@ from httpx import AsyncClient
 from ..config import plugin_config
 
 async def get_did() -> str:
-    if plugin_config.skland_use_web_timestamp:
-        async with AsyncClient() as client:
-            response = await client.get(plugin_config.skland_sm_api_endpoint)
-            response.raise_for_status()
-            response = response.json()
-            return response["dId"]
+    async with AsyncClient() as client:
+        response = await client.get(plugin_config.skland_sm_api_endpoint)
+        response.raise_for_status()
+        response = response.json()
+        return response["dId"]
